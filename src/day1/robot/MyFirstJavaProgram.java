0f9c935c218d20ee30564cf37ff48f21400eb2d4
League <league@Mac-mini.local>
2016-12-28 11:50:44 -0800

    coding was fun

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -6,10 +6,19 @@ import org.jointheleague.graphical.robot.Robot;
 public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
-		// START HERE
-
-	
-		
-		
-	}
+		// START HERE 
+Robot Noel=new Robot();
+for (int i = 0; i < 200; i++) {
+	Noel.setPenColor(200, 0, 0);
+Noel.setSpeed(200);
+Noel.penDown();
+Noel.move(200);
+Noel.turn(200);
+Noel.move(200);
+Noel.turn(200);
+Noel.move(200);
+Noel.turn(200);		
+Noel.move(200);
+}
+}
 }
